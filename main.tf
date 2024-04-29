c23ff79648d382f0557e7aa25b32576dc5735a46
DimitryZH <zhdm.dev@gmail.com>
2024-04-29 16:44:06 +0000

    main.tf changed

--- main.tf
+++ main.tf
@@ -2,24 +2,25 @@ data "aws_ami" "latest_ubuntu" {
   most_recent = true
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    name   = "description"
+    values = ["Canonical, Ubuntu, 24.04 LTS, amd64 noble image build on 2024-04-23"]
   }
 
   owners = ["099720109477"] # Canonical owner ID for Ubuntu AMIs
 }
 
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.latest_ubuntu.id
+  ami                    = "ami-04b70fa74e45c3917"  # Use the specific AMI ID
   instance_type          = var.server_size
   vpc_security_group_ids = [aws_security_group.web.id]
-  user_data              = filebase64("${path.module}/user_data.sh")
+  user_data              = file("${path.module}/user_data.sh")
 
   tags = {
     Name  = "${var.server_name}-WebServer"
     Owner = "Dmitry Zhuravlev"
   }
 }
+
 
 resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
 
